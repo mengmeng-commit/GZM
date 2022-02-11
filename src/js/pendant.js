9c1540da693f8e02c93a4376eb4328521cbfbcb6
Gao Zimeng <1318718554@qq.com>
2022-02-11 20:33:19 +0800

    version 1.0.0

---
+++ src/js/pendant.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded',function(){
+    let pendant=document.getElementById("pendant");
+    
+    window.addEventListener('scroll',function(){
+    let top=document.documentElement.scrollTop;
+    if(top>=687){
+        pendant.style.position="fixed";
+        pendant.style.left=1365+"px";
+        pendant.style.top=75+"px";
+    }
+    if(top<687){
+        pendant.style.position="absolute";
+        pendant.style.left=1190+"px";
+        pendant.style.top=531+"px";
+    }
+    })
+})
